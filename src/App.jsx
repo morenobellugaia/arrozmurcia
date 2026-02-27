3ee2323fb7beadc5e7cd1037ace350238edeb749
Antonio Moreno Belluga <amoreno@aurantis.es>
2026-02-27 16:54:59 +0100

    dist 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Ingredients from './pages/Ingredients'
 import Cooking from './pages/Cooking'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="ingredients" element={<Ingredients />} />
-        <Route path="cooking" element={<Cooking />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="ingredients" element={<Ingredients />} />
+      <Route path="cooking" element={<Cooking />} />
+    </Routes>
   )
 }
 
